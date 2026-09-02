95c1e7b428b1498a20d178f011e6964e7c258091
Wilian Domingos <wiliandomingos.93@gmail.com>
2026-09-01 22:22:30 -0300

    learn(ap01-dw): cria a tabela raw

---
+++ ap01-dw.sql
@@ -0,0 +1,31 @@
+-- CREATE SCHEMA IF NOT EXISTS raw;
+-- CREATE SCHEMA IF NOT EXISTS staging;
+-- CREATE SCHEMA IF NOT EXISTS dw;
+
+-- -- Conferencia
+-- SELECT schema_name
+-- FROM information_schema.schemata
+-- WHERE schema_name IN ('raw', 'staging', 'dw');
+
+-- DROP TABLE IF EXISTS raw.sales CASCADE;
+-- CREATE TABLE raw.sales (
+-- invoice_id TEXT,
+-- branch TEXT,
+-- city TEXT,
+-- customer_type TEXT,
+-- gender TEXT,
+-- product_line TEXT,
+-- unit_price TEXT,
+-- quantity TEXT,
+-- tax_5pct TEXT,
+-- total TEXT,
+-- sale_date TEXT,
+-- sale_time TEXT,
+-- payment TEXT,
+-- cogs TEXT,
+-- gross_margin_percentage TEXT,
+-- gross_income TEXT,
+-- rating TEXT
+-- );
+
+-- SELECT * FROM raw.sales;
